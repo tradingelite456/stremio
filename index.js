7567d579e6d4eed99b3fca3a7ea41924e4f1f801
Trading elite <tradingviewelite2@gmail.com>
2025-09-05 15:42:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const builder = new addonBuilder(manifest);
 
 // Mapping IMDb IDs → flux M3U8
 const streams = {
-    "tt0133093": [
-        { title: "Matrix HLS", url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }
+    "tt20969586": [
+        { title: "Thunderbolt", url: "https://pulse.topstrime.online/movie/986056/82nb0j/master.m3u8" }
     ],
     "tt0944947:1:1": [
         { title: "GoT S01E01 HLS", url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }
